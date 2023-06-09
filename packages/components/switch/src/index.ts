8426111835b8cfb451edd074d589cee2abe04d7b
‘caoyue’ <1310235550@qq.com>
2023-06-09 17:00:28 +0800

    #update 首次switch提交

---
+++ packages/components/switch/src/index.ts
@@ -0,0 +1,14 @@
+export {
+  OkuAvatar,
+  OkuAvatarFallback,
+  OkuAvatarImage,
+  createAvatarScope,
+} from './switch'
+export type {
+  AvatarFallbackProps,
+  AvatarProps,
+  AvatarImageProps,
+  OkuAvatarElement,
+  OkuAvatarFallbackElement,
+  OkuAvatarImageElement,
+} from './switch'
---
+++ packages/components/switch/src/switch.ts
@@ -0,0 +1,235 @@
+/* eslint-disable vue/one-component-per-file */
+import type { ComponentPublicInstance } from 'vue'
+import { computed, defineComponent, h, onMounted, onUnmounted, ref, watch, watchEffect } from 'vue'
+import type { ComponentPropsWithoutRef } from '@oku-ui/primitive'
+import { Primitive } from '@oku-ui/primitive'
+import type { Scope } from '@oku-ui/provide'
+import { createProvideScope } from '@oku-ui/provide'
+import { useCallbackRef } from '@oku-ui/use-callback-ref'
+
+function useImageLoadingStatus(src?: string) {
+  const loadingStatus = ref<ImageLoadingStatus>('idle')
+
+  onMounted(() => {
+    if (!src) {
+      loadingStatus.value = 'error'
+      return
+    }
+
+    let isMounted = true
+    const image = new window.Image()
+
+    const updateStatus = (status: ImageLoadingStatus) => () => {
+      if (!isMounted)
+        return
+      loadingStatus.value = status
+    }
+
+    loadingStatus.value = 'loading'
+    image.onload = updateStatus('loaded')
+    image.onerror = updateStatus('error')
+    image.src = src
+
+    onUnmounted(() => {
+      isMounted = false
+    })
+  })
+
+  return loadingStatus
+}
+
+/* -------------------------------------------------------------------------------------------------
+ * Avatar
+ * ----------------------------------------------------------------------------------------------- */
+
+const SWITCH_NAME = 'Switch'
+type ScopedProps<P> = P & { __scopeSwitch?: Scope }
+const [createSwitchContext, createSwitchScope] = createProvideScope(SWITCH_NAME)
+
+type SwitchContextValue = { checked: boolean; disabled?: boolean }
+const [SwitchProvider, useSwitchContext] = createSwitchContext<SwitchContextValue>(SWITCH_NAME)
+
+type SwitchElement = ComponentPropsWithoutRef<typeof Primitive.button>
+type PrimitiveButtonProps = ComponentPropsWithoutRef<typeof Primitive.button>
+interface SwitchProps extends PrimitiveButtonProps {
+  checked?: boolean
+  defaultChecked?: boolean
+  required?: boolean
+  onCheckedChange?(checked: boolean): void
+}
+
+const Switch = defineComponent({
+  name: SWITCH_NAME,
+  inheritAttrs: false,
+  setup(props, { attrs, slots, expose }) {
+    const { __scopeAvatar, ...avatarProps } = attrs as ScopedProps<AvatarProps>
+    const innerRef = ref()
+    const imageLoadingStatus = ref<ImageLoadingStatus>('idle')
+
+    SwitchProvider({
+      scope: __scopeAvatar,
+      imageLoadingStatus: imageLoadingStatus.value,
+      onImageLoadingStatusChange: (status: ImageLoadingStatus) => {
+        imageLoadingStatus.value = status
+      },
+    })
+
+    expose({
+      inferRef: computed(() => innerRef.value?.$el),
+    })
+
+    const originalReturn = () => h(
+      Primitive.span, {
+        ...avatarProps,
+        ref: innerRef,
+      },
+      slots.default && slots.default(),
+    )
+    return originalReturn as unknown as {
+      innerRef: AvatarElement
+    }
+  },
+})
+
+/* -------------------------------------------------------------------------------------------------
+ * AvatarImage
+ * ----------------------------------------------------------------------------------------------- */
+
+const IMAGE_NAME = 'AvatarImage'
+
+type AvatarImageElement = ComponentPropsWithoutRef<typeof Primitive.img>
+type PrimitiveImgProps = ComponentPropsWithoutRef<typeof Primitive.img>
+interface AvatarImageProps extends PrimitiveImgProps {
+  onLoadingStatusChange?: (status: ImageLoadingStatus) => void
+}
+
+const AvatarImage = defineComponent({
+  name: IMAGE_NAME,
+  inheritAttrs: false,
+  setup(props, { attrs, slots, expose }) {
+    const { __scopeAvatar, src, onLoadingStatusChange = () => {}, ...imageProps } = attrs as ScopedProps<AvatarImageProps>
+    const inject = useAvatarInject(IMAGE_NAME, __scopeAvatar)
+    const innerRef = ref<ComponentPublicInstance>()
+    const imageLoadingStatus = useImageLoadingStatus(src)
+
+    const handleLoadingStatusChange = useCallbackRef((status: ImageLoadingStatus) => {
+      onLoadingStatusChange(status)
+      inject.value.onImageLoadingStatusChange(status)
+    })
+
+    onMounted(() => {
+      if (imageLoadingStatus.value !== 'idle')
+        handleLoadingStatusChange(imageLoadingStatus.value)
+    })
+
+    watch(imageLoadingStatus, (newValue) => {
+      if (newValue !== 'idle')
+        handleLoadingStatusChange(newValue)
+    })
+
+    expose({
+      innerRef: computed(() => innerRef.value?.$el),
+    })
+
+    const originalReturn = () => imageLoadingStatus.value === 'loaded'
+      ? h(
+        Primitive.img, {
+          ...imageProps,
+          src,
+          ref: innerRef,
+        },
+        slots.default && slots.default(),
+      )
+      : null
+
+    return originalReturn as unknown as {
+      innerRef: AvatarImageElement
+    }
+  },
+})
+
+/* -------------------------------------------------------------------------------------------------
+ * AvatarFallback
+ * ----------------------------------------------------------------------------------------------- */
+
+const FALLBACK_NAME = 'AvatarFallback'
+
+type PrimitiveAvatarFallbackProps = ComponentPropsWithoutRef<typeof Primitive.span>
+type PrimitiveSpanElement = ComponentPropsWithoutRef<typeof Primitive.span>
+interface AvatarFallbackProps extends PrimitiveAvatarFallbackProps, PrimitiveSpanProps {
+  delayms?: number
+}
+const AvatarFallback = defineComponent({
+  name: FALLBACK_NAME,
+  inheritAttrs: false,
+  setup(props, { attrs, expose, slots }) {
+    const { __scopeAvatar, delayms, ...fallbackProps } = attrs as ScopedProps<AvatarFallbackProps>
+    const provide = useAvatarInject(FALLBACK_NAME, __scopeAvatar)
+    const canRender = ref(delayms === undefined)
+    const innerRef = ref<ComponentPublicInstance>()
+
+    onMounted(() => {
+      if (delayms === undefined)
+        canRender.value = true
+      else
+        canRender.value = false
+    })
+
+    onMounted(() => {
+      watchEffect(() => {
+        if (delayms !== undefined) {
+          const timerID = window.setTimeout(() => {
+            canRender.value = true
+          }, delayms)
+          return () => window.clearTimeout(timerID)
+        }
+      })
+    })
+
+    expose({
+      innerRef: computed(() => innerRef.value?.$el),
+    })
+
+    const originalReturn = () => {
+      return (canRender.value && (provide.value.imageLoadingStatus !== 'loaded'))
+        ? h(
+          Primitive.span, {
+            ...fallbackProps,
+            ref: innerRef,
+          },
+          slots.default && slots.default(),
+        )
+        : canRender.value
+    }
+
+    return originalReturn as unknown as {
+      innerRef: PrimitiveSpanElement
+    }
+  },
+})
+
+/* ----------------------------------------------------------------------------------------------- */
+
+const OkuAvatar = Avatar as typeof Avatar & (new () => { $props: ScopedProps<AvatarProps> })
+const OkuAvatarImage = AvatarImage as typeof AvatarImage & (new () => { $props: ScopedProps<AvatarImageProps> })
+const OkuAvatarFallback = AvatarFallback as typeof AvatarFallback & (new () => { $props: ScopedProps<AvatarFallbackProps> })
+
+type OkuAvatarElement = Omit<InstanceType<typeof Avatar>, keyof ComponentPublicInstance>
+type OkuAvatarImageElement = Omit<InstanceType<typeof AvatarImage>, keyof ComponentPublicInstance>
+type OkuAvatarFallbackElement = Omit<InstanceType<typeof AvatarFallback>, keyof ComponentPublicInstance>
+
+export {
+  OkuAvatar,
+  OkuAvatarImage,
+  OkuAvatarFallback,
+  createAvatarScope,
+}
+
+export type {
+  AvatarProps,
+  AvatarImageProps,
+  AvatarFallbackProps,
+  OkuAvatarElement,
+  OkuAvatarImageElement,
+  OkuAvatarFallbackElement,
+}
